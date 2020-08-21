b20101db0daeec38b9dd1aa69ec4e71edba3a5ca
annaadhiambo <annaadhiambo01@gmail.com>
2020-08-21 16:07:54 +0300

    generated strikethrough

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailComponent
+    QuotesDetailComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
